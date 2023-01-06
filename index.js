2ae1560ca0e890c221512c91bf044e911abac49f
Ganesh Aher <aherganesh194@gmail.com>
2023-01-06 16:12:09 +0530

    Add

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-indexedDB.js
+var seletedRow=null;
+function onFormSubmit(){
+
+}
+//retrieve the data
+function readFormData(){
+    varFormData={};
+    formData["userId"]=document.getElementById("userId").value;
+    formData["userName"]=document.getElementById("userName").value;
+    formData["email"]=document.getElementById("email").value;
+    formData["mobile"]=document.getElementById("mobile").value;
+    formData["Address"]=document.getElementById("Address").value;
+
+}
+//Insert the data
